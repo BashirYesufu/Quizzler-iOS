80657e366213309783f4b976a007fdb8051df1cb
Bashir Yesufu <yesufubashir@gmail.com>
2021-12-29 12:44:42 +0100

    Added dummy question and incremented the question while on screen

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -15,12 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+    "Four + Two is equal to Six",
+    "Five - Three is greater than One",
+    "Three + Eight is less than Ten"
+    ]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        updateUI()
     }
     
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+        questionNumber += 1
+    }
 }
 
