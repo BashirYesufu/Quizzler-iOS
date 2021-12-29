13d2e75802557f96d0fad800f6687cf9e4fb44d4
Bashir Yesufu <yesufubashir@gmail.com>
2021-12-29 20:20:39 +0100

    Added a question struct in a new file

---
+++ Quizzler/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by mac on 12/29/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
